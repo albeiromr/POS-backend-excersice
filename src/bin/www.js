c9f7b3d841d79e65d0cc4a8a5f70eda8ec7aa3a9
Albeiro Marin <xlandru@hotmail.com>
2021-08-01 13:32:31 -0500

    Create Entry point for our app

---
+++ src/bin/www.js
@@ -0,0 +1,10 @@
+const app = require("../app");
+const dotenv = require("dotenv");
+
+dotenv.config({path: `${__dirname}/../../.env`});
+
+const port = process.env.port || 5000;
+
+app.listen(port, () => {
+    console.log(`Escuchando en puerto ${port}`)
+});
